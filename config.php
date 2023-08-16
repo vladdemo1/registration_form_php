d352ae41e891aa94e7066314854d5240dcfc156d
vladdemo1 <vladdemo140@gmail.com>
2023-08-16 12:33:15 +0300

    Added social buttons for share

--- config.php
+++ config.php
@@ -7,4 +7,6 @@ return $config = [
     'dbname'=>'registration',
     'dbpass'=>'password',
     'dbuser'=>'vladdemo',
+    'message'=>'Check out this Meetup with SoCal AngularJS!',
+    'address'=>'!1m18!1m12!1m3!1d3303.761723587065!2d-118.34625852349969!3d34.101244073138524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2bf20e4c82873%3A0x14015754d926dadb!2zNzA2MCBIb2xseXdvb2QgQmx2ZCwgTG9zIEFuZ2VsZXMsIENBIDkwMDI4LCDQodCo0JA!5e0!3m2!1sru!2sua!4v1692171050681!5m2!1sru!2sua',
 ];
